e600c47ef430edfa2d45bd9253cfd884bf079fdf
unknown <gavrisnazar981@gmail.com>
2024-10-11 00:59:00 +0300

    labyrinth solver v2

--- labyrinth.py
+++ labyrinth.py
@@ -1,4 +1,5 @@
 import time
+import queue
 
 
 class LabyrinthSolver:
@@ -20,7 +21,7 @@ class LabyrinthSolver:
         :param y: i (the y coordinate)
         :return: bool (True if the solver can, False otherwise)
         """
-        if 0 <= x < len(self.labyrinth) and 0 <= y < len(self.labyrinth) and self.labyrinth[x][y] == 0:
+        if 0 <= x < len(self.labyrinth) and 0 <= y < len(self.labyrinth[0]) and self.labyrinth[x][y] == 0:
             return True
         else:
             return False
@@ -38,36 +39,38 @@ class LabyrinthSolver:
         print()
         time.sleep(1)
 
-    def find_path(self, x=None, y=None):
+    def find_path(self):
         """
-        Finds the path from start to end of the labyrinth. Using backtracking.
+        Finds the path from start to end of the labyrinth. Using backtracking and BFS.
         :param x: int (x coordinate)
         :param y: i (y coordinate)
         :return: bool(True if path found, False if not.)
         """
-        if x is None and y is None:
-            x = self.start[0]
-            y = self.start[1]
-
-        if x == self.end[0] and y == self.end[1]:
-            print("Yey, yupi!! Path found.")
-            return True
-
-        self.print_labyrinth()
-
+        moves = queue.Queue()
+        moves.put([self.start])
         directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
-        for direction in directions:
-            new_x = x + direction[0]
-            new_y = y + direction[1]
 
-            if self.can_move(new_x, new_y):
-                self.labyrinth[x][y] = '/'
-                self.labyrinth[new_x][new_y] = 2
-                if self.find_path(new_x, new_y):
-                    return True
-            else:
-                self.labyrinth[x][y] = 'x'
-                return False
+        while not moves.empty():
+            path = moves.get()
+            x, y = path[-1]
+            if x == self.end[0] and y == self.end[1]:
+                for x, y in path:
+                    labyrinth[x][y] = "/"
+                print("Yey, yupi!! Path found.")
+                self.print_labyrinth()
+                return True
+
+            for direction in directions:
+                new_x, new_y = x + direction[0], y + direction[1]
+
+                if self.can_move(new_x, new_y):
+                    new_path = list(path)
+                    new_path.append((new_x, new_y))
+                    moves.put(new_path)
+
+                    self.labyrinth[new_x][new_y] = 2
+                    self.print_labyrinth()
+        return False
 
 
 labyrinth = [
@@ -75,14 +78,13 @@ labyrinth = [
     [1, 0, 1, 0, 1],
     [1, 0, 0, 0, 0],
     [1, 0, 1, 1, 0],
-    [1, 0, 1, 0, 0]
+    [1, 0, 1, 1, 0]
 ]
 
 start = (0, 0)
 end = (4, 3)
 
 solver = LabyrinthSolver(labyrinth, start, end)
-solver.find_path()
 
 if solver.find_path() is False:
     print("Bro, labyrinth is a bit impossible.-_-")
